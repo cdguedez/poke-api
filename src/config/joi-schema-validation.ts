9826e639a85e88dffa638d32cb0dbae83b9bbedf
Carlos Guedez <cdgr.ing@gmail.com>
2024-06-29 15:43:52 -0400

    add test DB connection

--- src/config/joi-schema-validation.ts
+++ src/config/joi-schema-validation.ts
@@ -1,7 +1,11 @@
 import * as joi from 'joi';
 
 export const JoiSchemaValidation = joi.object({
-  MONGO_URL: joi.string().required(),
+  MONGO_URL: joi
+    .string()
+    .default(
+      'mongodb://mongo:POSuQcsuMKspgKmWExLFWgsEzpuHvHPG@roundhouse.proxy.rlwy.net:50526',
+    ),
   PORT: joi.number().default(3000),
   POKEAPI_URL: joi.string().default('https://pokeapi.co/api/v2/pokemon'),
 });
